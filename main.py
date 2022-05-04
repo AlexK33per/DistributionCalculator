e4e927861a2e7e0f63f7c9e5cdeb25f6f8f21c53
alexk33per <aalonsopersonal@gmail.com>
2022-05-04 10:30:19 +0200

    done random gen with exp distribution

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
+import math as ma
+
 # Función que elige entre las dos distribuciones
+
 
 def chooseDistribution():
     option = -1
-    while(option > 1 or option < 0):    
-        print("Pick Poisson or Exponential distribution")
+    while(option > 1 or option < 0):
+        print("Pick a Distribution to generate a random number:")
         print("0: Poisson")
         print("1: Exponential")
         sel = input()
         option = int(sel)
     return option
 
+# Función con el display para introducir la valor de 0 a 1.
+
+
+def inputValue():
+    print("Enter a valid number (from 0 to 1):")
+    value = input()
+    return value
+
+
+# Función con el display para introducir la lambda
+
+
+def inputLambda():
+    print("Introduce una Lambda para la distribución")
+    pick = input()
+    return pick
+
 # Funcion que ejecuta la distribución de Poisson
 
-def poissonDistribution():
-    print("To-Do")
 
-
+def poissonDistributionRandomGen(lambdaparam):
+    print("todo")
 # Función que ejecuta la distribución de Exponencial
 
-def exponentialDistribution():
-    print("To-Do")
+
+def exponentialDistributionRandomGen(lambdaparam):
+    logparam = inputValue()
+    division = float(1 / float(lambdaparam))
+    result = division * float(ma.log(float(logparam)))
+    print(ma.fabs(result))
 
 # Función que contiene la totalidad del programa
 
+
 def main():
-    val = int(chooseDistribution())
-    poissonDistribution() if val == 0 else exponentialDistribution()
+    lamb = inputLambda()
+    exponentialDistributionRandomGen(float(lamb))
+
 
 main()
