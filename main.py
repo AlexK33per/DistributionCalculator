cade91cdcc8491d0d278be8a523c02a2808fa74e
Alejandro Alonso <aalonsopersonal@gmail.com>
2022-05-11 13:13:04 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import math as ma
-from re import U
 import numpy as np
+import pandas as excel
 
 # Función que elige entre las dos distribuciones
-
 
 def chooseDistribution():
     option = -1
     while(option > 1 or option < 0):
         print("Pick a Distribution to generate a random number:")
         print("0: Poisson")
-        print("1: Exponential")
+        print("1: Exponencial")
         sel = input()
         option = int(sel)
     return option
 
 # Función con el display para introducir la valor de 0 a 1.
-
 
 def inputValue():
     print("Enter a valid number (from 0 to 1):")
@@ -26,14 +24,16 @@ def inputValue():
 
 # Función con el display para introducir la lambda
 
-
-def inputLambda():
-    print("Introduce una Lambda para la distribución")
-    pick = input()
+def inputParam(value):
+    if(value == 0):
+        print("Introduce una Lambda para la distribución de Poisson:")
+        pick = input()
+    elif (value == 1):
+        print("Introduce una Beta para la distribución Exponencial:")
+        pick = input()
     return pick
 
 # Funcion que ejecuta la distribución de Poisson
-
 
 def poissonDistributionRandomGen(lambdaparam):
     u = 0
@@ -48,34 +48,31 @@ def poissonDistributionRandomGen(lambdaparam):
             b = arr[i] * b
             i = i + 1
             n = n + 1
-        print(n)
+        sol[u] = n
         u = u + 1
 
 # Función que ejecuta la distribución de Exponencial
 
-
-def exponentialDistributionRandomGen(lambdaparam):
+def exponentialDistributionRandomGen(betaparam):
     i = 0
     while(i < 100):
         arr = np.random.uniform(0, 1, 1)
         logparam = arr[0]
-        division = float(1 / float(lambdaparam))
+        division = float(1 / float(betaparam))
         result = division * float(ma.log(float(logparam)))
         print(abs(result))
         i = i + 1
 
 # Función que contiene la totalidad del programa
 
-
 def main():
-    lamb = inputLambda()
     val = int(chooseDistribution())
     if val == 0:
         print("El resultado es:")
-        # print(poissonDistributionRandomGen(lamb))
         poissonDistributionRandomGen(lamb)
     else:
         print("El resultado es:")
         exponentialDistributionRandomGen(lamb)
 
 main()
+
