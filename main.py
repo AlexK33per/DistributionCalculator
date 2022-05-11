57f9c3836d7656127a1895d0707cf1801d7a77e2
alexk33per <aalonsopersonal@gmail.com>
2022-05-11 14:40:51 +0200

    arreglos para miñano

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import math as ma
 import numpy as np
 import pandas as excel
+from prettytable import PrettyTable
 
 # Función que elige entre las dos distribuciones
 
 def chooseDistribution():
     option = -1
-    while(option > 1 or option < 0):
-        print("Pick a Distribution to generate a random number:")
-        print("0: Poisson")
-        print("1: Exponencial")
-        sel = input()
-        option = int(sel)
+    print("Introduce un número para seleccionar la distribución generadora de un número aleatorio:")
+    print("0: Poisson")
+    print("1: Exponencial")
+    print("Cualquier otro número cierra el programa")
+    sel = input()
+    option = int(sel)
     return option
 
 # Función con el display para introducir la valor de 0 a 1.
@@ -36,6 +37,7 @@ def inputParam(value):
 # Funcion que ejecuta la distribución de Poisson
 
 def poissonDistributionRandomGen(lambdaparam):
+    sol = list()
     u = 0
     while(u < 100):
         a = pow(ma.e, -float(lambdaparam))
@@ -48,31 +50,43 @@ def poissonDistributionRandomGen(lambdaparam):
             b = arr[i] * b
             i = i + 1
             n = n + 1
-        sol[u] = n
+        sol.append(n)    
         u = u + 1
+    return sol
 
 # Función que ejecuta la distribución de Exponencial
 
 def exponentialDistributionRandomGen(betaparam):
+    sol = list()
     i = 0
     while(i < 100):
         arr = np.random.uniform(0, 1, 1)
         logparam = arr[0]
         division = float(1 / float(betaparam))
         result = division * float(ma.log(float(logparam)))
-        print(abs(result))
+        sol.append(abs(result))
         i = i + 1
+    return sol
 
 # Función que contiene la totalidad del programa
 
 def main():
     val = int(chooseDistribution())
-    if val == 0:
-        print("El resultado es:")
-        poissonDistributionRandomGen(lamb)
-    else:
-        print("El resultado es:")
-        exponentialDistributionRandomGen(lamb)
+    exp = list()
+    poisson = list()
+    while(val > -1 and val < 2):
+        if val == 0:
+            lamb = inputParam(val)
+            print("El resultado es:")
+            poisson = poissonDistributionRandomGen(lamb)
+            tabulate([])
+        elif val == 1:
+            beta = inputParam(val)
+            print("El resultado es:")
+            exp = exponentialDistributionRandomGen(beta)
+            tabulate(list(range(100)), beta, headers = ["Xi", "Número generado"])
+    data = excel.DataFrame({"Poisson":poisson, "Exponencial":exp})
+    data.to_excel("Datos_Generados.xlsx", index = False)
 
 main()
 
